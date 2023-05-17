ec6e0579c60f64c8a27b5b096714aa18c8594566
h4nzi <94918729+h4nzi@users.noreply.github.com>
2023-05-17 11:44:54 +0200

    1.0.4

--- netspeed.py
+++ netspeed.py
@@ -1,6 +1,4 @@
 import time
-import os
-import sys
 import xbmc
 import xbmcgui
 import xbmcaddon
@@ -55,4 +53,71 @@ if __name__ == '__main__':
             break
         tx_prev = tx
         rx_prev = rx
+        
+        
+# import datetime
+# import csv
+# import xbmc
+# import xbmcgui
+# import time
+# import os
+# import sys
+# import xbmcaddon
+# import json
+
+# Plugin Info
+
+# addon_id = 'service.netspeed.monitor'
+# selfAddon = xbmcaddon.Addon(addon_id)
+# __version__ = selfAddon.getAddonInfo('version')
+# monitor = xbmc.Monitor()
+
+   
+# if selfAddon.getSetting('rozhrani') == '0':
+    # interface="eth0"
+    # print("interface = eth0")
+# elif selfAddon.getSetting('supervision_mode') == '1':
+    # interface="wlan0"
+    # print("interface = wlan0")
+# else:
+    # print("Neznámé rozhraní a nebo není povoleno")
+
+
+
+# def get_bytes(t, iface=interface):
+    # with open('/sys/class/net/' + iface + '/statistics/' + t + '_bytes', 'r') as f:
+        # data = f.read();
+    # return int(data)
+
+# if __name__ == '__main__':
+    # (tx_prev, rx_prev) = (0, 0)
+    # monitor = xbmc.Monitor()
+    # while not monitor.abortRequested():
+        # try:
+            # tx = get_bytes('tx')
+            # rx = get_bytes('rx')
+        # except IOError:
+            # break
+        # if tx_prev > 0:
+            # tx_speed = (tx - tx_prev) / 2
+            # tx_b = str(round(tx_speed / 132072, 2))
+            # xbmcgui.Window(10000).setProperty('TX', tx_b)
+        # if rx_prev > 0:
+            # rx_speed = (rx - rx_prev) / 2
+            # rx_b = str(round(rx_speed / 132072, 2))
+            # xbmcgui.Window(10000).setProperty('RX', rx_b)
+            
+            
+            # filename = 'resources/data/speed.csv'
+            # now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            # with open(filename, mode='a', newline='') as file:
+                # writer = csv.writer(file)
+                # writer.writerow([now, rx_b])
+                
+        # if monitor.waitForAbort(2):
+            # xbmc.log("*********Netspeed Abort Called*****************", 2)
+            # break
+        # tx_prev = tx
+        # rx_prev = rx
+
         
